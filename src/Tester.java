b23edde80942e9b3b385d73cc9a1c97d34af589a
pantilimonbianca <pantilimonbianca00@gmail.com>
2022-11-03 14:26:13 +0100

    final commit

---
+++ src/Tester.java
@@ -0,0 +1,7 @@
+public class Tester {
+    public static void main(String[] args) {
+        House house1= new House("street paravelli n.9 ", 6);
+        House house2= new House("street maroncelli n.4 ", 8);
+
+    }
+}
